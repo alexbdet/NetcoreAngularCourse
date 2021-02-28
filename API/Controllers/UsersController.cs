ebc4d4b34b13edbb2dd93f36de054c537da79806
Alexandre Beaudet <RedOwl0v0@gmail.com>
2021-02-28 04:23:16 +0100

    Project init & AppUser entity

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        /// <summary>
+        /// Gets a singular user based on an id.
+        /// </summary>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user is null)
+                return NotFound();
+                
+            return user;
+        }
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+namespace API.Entities
+{
+    public class AppUser
+    {
+        /// <summary>
+        /// Represents the AppUser's identification number.
+        /// </summary> 
+        public int Id { get; set; }
+        
+        /// <summary>
+        /// Represents the AppUser's username. 
+        /// </summary>
+        public string UserName { get; set; }
+    }
+}

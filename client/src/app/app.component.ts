56e8c80392ab1cf4963b9663ef3b3c05d827c3fe
Alexandre Beaudet <RedOwl0v0@gmail.com>
2021-03-01 20:57:24 +0100

    Home / Register components design

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
@@ -13,29 +12,15 @@ export class AppComponent implements OnInit {
   users: any;
 
   constructor(
-    private http: HttpClient,
     private accountService: AccountService
   ) {}
 
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
   }
 
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user'));
     this.accountService.setCurrentUser(user);
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(
-      (response) => {
-        this.users = response;
-        console.log(this.users);
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
   }
 }

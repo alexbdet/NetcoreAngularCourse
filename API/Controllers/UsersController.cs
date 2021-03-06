60c692181ff09fb6948945428cecb908af5333f6
Alexandre Beaudet <RedOwl0v0@gmail.com>
2021-03-06 03:31:07 +0100

    Photo upload API endpoint

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,9 +2,12 @@ using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using API.DTOs;
+using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -14,9 +17,11 @@ namespace API.Controllers
     {
         private readonly IUserRepository _userRepo;
         private readonly IMapper _mapper;
+        private readonly IPhotoService _photoService;
 
-        public UsersController(IUserRepository userRepo, IMapper mapper)
+        public UsersController(IUserRepository userRepo, IMapper mapper, IPhotoService photoService)
         {
+            _photoService = photoService;
             _mapper = mapper;
             _userRepo = userRepo;
         }
@@ -33,7 +38,7 @@ namespace API.Controllers
         /// <summary>
         /// Gets a singular user based on an id.
         /// </summary>
-        [HttpGet("{username}")]
+        [HttpGet("{username}", Name = "GetUser")]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             var user = await _userRepo.GetMemberByUsernameAsync(username);
@@ -45,8 +50,7 @@ namespace API.Controllers
         [HttpPut]
         public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdate)
         {
-            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            var user = await _userRepo.GetUserByUsernameAsync(username);
+            var user = await _userRepo.GetUserByUsernameAsync(User.GetUsername());
 
             _mapper.Map(memberUpdate, user);
             _userRepo.Update(user);
@@ -54,6 +58,29 @@ namespace API.Controllers
             if (await _userRepo.SaveAllAsync()) return NoContent();
 
             return BadRequest("Failed to update user");
+        }
+
+        [HttpPost("add-photo")]
+        public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+        {
+            var user = await _userRepo.GetUserByUsernameAsync(User.GetUsername());
+            var result = await _photoService.AddPhotoAsync(file);
+
+            if (result.Error is not null) return BadRequest(result.Error.Message);
+
+            var photo = new Photo
+            {
+                Url = result.SecureUrl.AbsoluteUri,
+                PublicId = result.PublicId
+            };
+
+            photo.IsMain = user.Photos.Count == 0;
+            user.Photos.Add(photo);
+
+            if (await _userRepo.SaveAllAsync())
+                return CreatedAtRoute("GetUser", new { username = user.UserName }, _mapper.Map<Photo, PhotoDto>(photo));
+
+            return BadRequest("Problem adding photo");
         }
     }
 }
---
+++ API/Services/PhotoService.cs
@@ -0,0 +1,50 @@
+using System.Threading.Tasks;
+using API.Helpers;
+using API.Interfaces;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+
+namespace API.Services
+{
+    public class PhotoService : IPhotoService
+    {
+        private readonly Cloudinary _cloundinary;
+        public PhotoService(IOptions<CloudinarySettings> options)
+        {
+            var acc = new Account(
+                options.Value.CloudName,
+                options.Value.ApiKey,
+                options.Value.ApiSecret
+            );
+
+            _cloundinary = new Cloudinary(acc);
+        }
+
+        public async Task<ImageUploadResult> AddPhotoAsync(IFormFile file)
+        {
+            var uploadResult = new ImageUploadResult();
+
+            if (file.Length > 0)
+            {
+                using var stream = file.OpenReadStream();
+                var uploadParams = new ImageUploadParams
+                {
+                    File = new FileDescription(file.FileName, stream),
+                    Transformation = new Transformation().Height(500).Width(500).Crop("fill").Gravity("face")
+                };
+
+                uploadResult = await _cloundinary.UploadAsync(uploadParams);
+            }
+
+            return uploadResult;
+        }
+
+        public async Task<DeletionResult> DeletePhotoAsync(string publicId)
+        {
+            var deletionParams = new DeletionParams(publicId);
+            return await _cloundinary.DestroyAsync(deletionParams);
+        }
+    }
+}

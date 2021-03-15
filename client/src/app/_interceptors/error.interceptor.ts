53b54a1ffc192e23a766ce6287f93632e1bc559e
Alexandre Beaudet <RedOwl0v0@gmail.com>
2021-03-15 08:20:04 +0100

    Fixed validation handling

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -23,6 +23,9 @@ export class ErrorInterceptor implements HttpInterceptor {
         if (error) {
           switch (error.status) {
             case 400:
+              console.log(error);
+              console.log(Array.isArray(error));
+              console.log(Array.isArray(error.error));
               if (error.error.errors) {
                 const modalStateErrors = [];
                 for (const key in error.error.errors) {
@@ -31,9 +34,19 @@ export class ErrorInterceptor implements HttpInterceptor {
                   }
                 }
                 throw modalStateErrors.flat();
+              } else if (Array.isArray(error.error)) {
+                const modalStateErrors = [];
+                const errors: { description: string }[] = error.error;
+
+                errors.forEach((e) =>  {
+                  modalStateErrors.push(e.description);
+                });
+
+                throw modalStateErrors.flat();
               } else if (typeof (error.error) === 'object') {
                 this.toastrError(error.statusText, error.status, 'Bad Request');
-              } else {
+              }
+              else {
                 this.toastrError(error.error, error.status, 'Bad Request');
               }
               break;
